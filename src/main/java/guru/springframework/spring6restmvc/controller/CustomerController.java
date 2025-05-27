a8fed9cd53245b16acc61569146d76f47792b450
shindu-aw <adamwoy1999@gmail.com>
2025-05-27 16:48:29 +0200

    Lesson 62 task finished

---
+++ src/main/java/guru/springframework/spring6restmvc/controller/CustomerController.java
@@ -0,0 +1,31 @@
+package guru.springframework.spring6restmvc.controller;
+
+import guru.springframework.spring6restmvc.model.Customer;
+import guru.springframework.spring6restmvc.services.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/customer")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Customer> listCustomers() {
+        return customerService.listCustomers();
+    }
+
+    @RequestMapping(value = "{customerId}", method = RequestMethod.GET)
+    public Customer getCustomerById(@PathVariable("customerId") UUID customerId) {
+        return customerService.getCustomerById(customerId);
+    }
+
+}
