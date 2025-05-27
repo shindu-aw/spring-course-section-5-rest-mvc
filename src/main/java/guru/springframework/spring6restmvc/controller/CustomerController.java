30323f2fd460a5ac6807c301609e59f8d3770b59
shindu-aw <adamwoy1999@gmail.com>
2025-05-27 18:29:18 +0200

    Lesson 68 task finished - HTTP PATCH

--- src/main/java/guru/springframework/spring6restmvc/controller/CustomerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/CustomerController.java
@@ -18,6 +18,12 @@ public class CustomerController {
 
     private final CustomerService customerService;
 
+    @PatchMapping("{customerId}")
+    public ResponseEntity patchCustomerById(@PathVariable("customerId") UUID customerId,
+                                            @RequestBody Customer customer) {
+        customerService.patchCustomerById(customerId, customer);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
 
     @DeleteMapping("{customerId}")
     public ResponseEntity deleteCustomerById(@PathVariable("customerId") UUID customerId) {
