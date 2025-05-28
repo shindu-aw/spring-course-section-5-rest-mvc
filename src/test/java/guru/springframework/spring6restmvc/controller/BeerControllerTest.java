8eda547e966146185effb957e0ebd986f2b93f36
shindu-aw <adamwoy1999@gmail.com>
2025-05-28 21:10:07 +0200

    Lesson 71 task finished - MockMVC config

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -1,23 +1,32 @@
 package guru.springframework.spring6restmvc.controller;
 
+import guru.springframework.spring6restmvc.services.BeerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@WebMvcTest(BeerController.class) // this is indicating that this is a Test Splice
 class BeerControllerTest {
 
     @Autowired
-    BeerController beerController;
+    MockMvc mockMvc; // this sets up Spring MockMVC
+
+    @MockitoBean
+    BeerService beerService;
 
     @Test
-    void getBeerById() {
+    void getBeerById() throws Exception {
 
-        System.out.println(beerController.getBeerById(UUID.randomUUID()));
+        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
 
     }
 }
