c83f099a8ba86951b21ccec308b913cb6a3005b1
shindu-aw <adamwoy1999@gmail.com>
2025-06-01 17:21:41 +0200

    Lesson 115 - JPA (database) validation
    - added validation for Beer entity to match the Beer DTO
    - changed the BeerRepositoryTest to pass with newly added constraints

--- src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,9 +1,12 @@
 package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,8 +20,18 @@ class BeerRepositoryTest {
     @Test
     void testSaveBeer() {
         Beer savedBeer = beerRepository.save(
-                Beer.builder().beerName("My Beer").build()
+                Beer.builder()
+                        .beerName("My Beer")
+                        .beerStyle(BeerStyle.PALE_ALE)
+                        .upc("23423424234")
+                        .price(new BigDecimal("11.99"))
+                        .build()
         );
+
+        // * sometimes, Hibernate will flush and batch, but the session is ending too quickly, so to actually
+        // * see the error from the constraint validation, we need to explicitly flush the repository
+        // ? .flush() tells Hibernate to immediately write to the database
+        beerRepository.flush();
 
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
