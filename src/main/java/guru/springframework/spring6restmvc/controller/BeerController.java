0c9c0316718e6b8b8b53c3b76285b312c77fad2a
shindu-aw <adamwoy1999@gmail.com>
2025-06-05 20:07:37 +0200

    Lesson 155 - capture query parameters with Spring MVC
    - changed BeerController's listBeers() to listBeers(@RequestParam(required = false) String beerName)
    - changed BeerControllerIT tests to be compatible with the modified listBeers(...) method

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -64,7 +64,7 @@ public class BeerController {
     }
 
     @GetMapping(BEER_PATH)
-    public List<BeerDTO> listBeers() {
+    public List<BeerDTO> listBeers(@RequestParam(required = false) String beerName) {
         return beerService.listBeers();
     }
 
